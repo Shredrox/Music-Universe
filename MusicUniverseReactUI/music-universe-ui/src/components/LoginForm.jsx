0a5330bf6dae1947bc0dba0b95503ec5c5ac76de
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-04-17 11:18:57 +0300

    Testing login styling

--- MusicUniverseReactUI/music-universe-ui/src/components/LoginForm.jsx
+++ MusicUniverseReactUI/music-universe-ui/src/components/LoginForm.jsx
@@ -26,12 +26,12 @@ export function LoginForm(){
     return (
         <form className="login">
             <label htmlFor="name">Name: </label>
-            <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}/><br/>
+            <input className='input' type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}/><br/>
             <label htmlFor="email">Email: </label>
-            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/><br/>
+            <input className='input' type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/><br/>
             <label htmlFor="email">Password: </label>
-            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/><br/>
-            <button type="submit" onClick={submitData}>Submit</button><br/><br/><br/>
+            <input className='input' type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/><br/>
+            <button className='glow-on-hover' type="submit" onClick={submitData}>Submit</button><br/><br/><br/>
         </form>
     )
 }
