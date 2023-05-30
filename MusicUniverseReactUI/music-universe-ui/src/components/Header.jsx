1d7e57d7b3edcd67b71774f1ac155e131d5e626a
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-05-30 13:06:45 +0300

    Added login/signup buttons

--- MusicUniverseReactUI/music-universe-ui/src/components/Header.jsx
+++ MusicUniverseReactUI/music-universe-ui/src/components/Header.jsx
@@ -4,21 +4,29 @@ import myImage from '../assets/homeBackground.png';
 import { Link } from "react-router-dom";
 
 export function Header(){
+
     return(
         <header className="headerBar">
             <nav className="navigation">
                 <ul className="navList">
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/catalog">Catalog</Link></li>
                     <li></li>
                 </ul>
             </nav>
 
             <input className="searchBar" type="text" placeholder="Search.."/>
 
-            <div className="profileBar">
-                <h3 className="profileName">Shredrox</h3>
-                <img className="profileImg" src={myImage}  alt=""/>
+            <div className="button-container">
+                
+                <Link to="/login" style={{textDecoration: 'none'}}>
+                    <button className="login-button" role="button">
+                        <span>Login</span>
+                    </button>
+                </Link>
+                <button className="login-button" role="button">
+                    <span>Sign Up</span>
+                </button>
             </div>
         </header>
     )
