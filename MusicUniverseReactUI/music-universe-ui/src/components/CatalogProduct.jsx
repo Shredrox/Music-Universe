99c71a23aa0bf3a55f63253d43cc35505fc28351
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-05-31 18:03:54 +0300

    Working on catalog

---
+++ MusicUniverseReactUI/music-universe-ui/src/components/CatalogProduct.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import featuredProductImage from '../assets/mishaGuitar.png';
+
+export function CatalogProduct(){
+    return (
+        <div className="productCard">
+            <div className="product">
+                <img className="featuredProductImg" src={featuredProductImage} alt="" />
+            </div>
+            <p className="productName">
+                Guitar 1
+            </p>
+        </div>
+    )
+}
--- MusicUniverseReactUI/music-universe-ui/src/pages/Catalog.jsx
+++ MusicUniverseReactUI/music-universe-ui/src/pages/Catalog.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { CategoriesTab } from "../components/CategoriesTab";
+import { CatalogProduct } from "../components/CatalogProduct";
 
 export function Catalog(){
     return (
         <div id="catalog-section">
             <div id="catalog-container">
                 <CategoriesTab/>
+                <div className="catalog-products-container">
+                    <CatalogProduct/>
+                    <CatalogProduct/>
+                    <CatalogProduct/>
+                    <CatalogProduct/>
+                    <CatalogProduct/>
+                    <CatalogProduct/>
+                    
+
+                </div>
             </div>
         </div>
     )
