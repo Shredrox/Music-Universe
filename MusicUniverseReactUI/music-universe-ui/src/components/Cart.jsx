8a74376479c9f6672cbc1befe7844213f261acee
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-03 21:26:36 +0300

    Added cart icon and calculating total cart price

--- MusicUniverseReactUI/music-universe-ui/src/components/Cart.jsx
+++ MusicUniverseReactUI/music-universe-ui/src/components/Cart.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { CartProduct } from "./CartProduct";
+import cartIcon from '../assets/cartBtnIcon.png';
 
 export function Cart({products, toggleCart}){
 
     const productsInCart = products.filter((product) => product.inCart);
+    const totalPrice = productsInCart.reduce((acc, product) => acc + product.price, 0);
 
     return (
         <div id="cart-container">
@@ -13,7 +15,9 @@ export function Cart({products, toggleCart}){
                 )}
             </div>
             <div id="cart-summary">
-                Your Cart Bro
+                <span>Your Cart<img src={cartIcon} alt="" /></span> 
+                <label>Total Price: ${totalPrice}</label>
+                <button className="nav-button">Checkout</button>
             </div>
         </div>
     )
