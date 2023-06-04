72c6dc3a70868fe07bb5e61c1e5b69ffd4e779d1
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-04 22:38:34 +0300

    Added onClick to the search result for product navigating

--- MusicUniverseReactUI/music-universe-ui/src/components/SearchBarResultList.jsx
+++ MusicUniverseReactUI/music-universe-ui/src/components/SearchBarResultList.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 export const SearchBarResultList = ({filteredProducts}) => {
+  const navigate = useNavigate(); 
+
   return (
     <div>
       {filteredProducts.length > 0 &&
       <div className="search-results">
           {filteredProducts.map((product) =>
-              <div key={product.id}>
+              <div onClick={() => {navigate(`/product/${product.id}`) }} className='search-result' key={product.id}>
                 <img className='smol' src={product.image} alt="" />
                 {product.name}
               </div>
