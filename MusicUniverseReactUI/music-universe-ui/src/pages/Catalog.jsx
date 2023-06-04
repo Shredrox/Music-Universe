4e26b569b5c4da9c2695f3480c7459919530dbdf
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-04 16:14:33 +0300

    Added fetching products in catalog

--- MusicUniverseReactUI/music-universe-ui/src/pages/Catalog.jsx
+++ MusicUniverseReactUI/music-universe-ui/src/pages/Catalog.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import { CategoriesTab } from "../components/CategoriesTab";
 import { CatalogTab } from "../components/CatalogTab";
+import { useState, useEffect } from "react";
 
-export function Catalog({products, toggleCart}){
+export function Catalog({toggleCart}){
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const result = await fetch('http://localhost:5000/products/');
+        const data = await result.json();
+        
+        return data;
+    }
+    
+    const getProducts = async () => {
+        const getProductsFromServer = await fetchProducts();
+        setProducts(getProductsFromServer);
+    }
+    
+    useEffect(() => {
+        getProducts();
+    }, [])
+
     return (
         <div id="catalog-section">
             <div id="catalog-container">
