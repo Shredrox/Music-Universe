0851e806fec077c6e7b29c4a3ae1049d4db69ae4
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-05 14:58:56 +0300

    Added 'added to cart' message to catalog product

--- MusicUniverseReactUI/music-universe-ui/src/components/CatalogProduct.jsx
+++ MusicUniverseReactUI/music-universe-ui/src/components/CatalogProduct.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export function CatalogProduct({product, toggleCart}){
     const navigate = useNavigate(); 
+    const [showCartMsg, setShowCartMsg] = useState(false);
 
     function stopPropagation(e) {
         e.stopPropagation();
+    }
+
+    function toggleCartMsg(){
+        setShowCartMsg(true);
+        setTimeout(() =>{
+          setShowCartMsg(false);
+        }, 3000);
     }
 
     return (
@@ -19,11 +28,19 @@ export function CatalogProduct({product, toggleCart}){
             <p className="productName">
                 Price: ${product.price}
             </p>
-            <button 
-                className="catalog-cart-button" 
-                onClick={(e) =>{stopPropagation(e); toggleCart(product.id, 1)} }>
-                Add To Cart
-            </button>
+            <div className='catalog-product-add-to-cart-container'>
+                <button 
+                    className="catalog-cart-button" 
+                    onClick={(e) =>{stopPropagation(e); toggleCart(product.id, 1); toggleCartMsg()} }>
+                    Add To Cart
+                </button>
+                {showCartMsg &&
+                    <div className="add-to-cart-msg">
+                    Added To Cart!
+                    </div>
+                }
+            </div>
+            
         </div>
     )
 }
