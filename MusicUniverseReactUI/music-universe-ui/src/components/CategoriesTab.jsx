9d8bf64ff686929a2a0c3d22cffbbe8bf99b57fd
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-06-06 19:27:55 +0300

    Changed categories

--- MusicUniverseReactUI/music-universe-ui/src/components/CategoriesTab.jsx
+++ MusicUniverseReactUI/music-universe-ui/src/components/CategoriesTab.jsx
@@ -21,14 +21,11 @@ export function CategoriesTab({products, setFilteredProducts, setFilterOn}){
     }
 
     const buttonData = [
-        { id: 1, category: "6 String" },
+        { id: 1, category: "Electric" },
         { id: 2, category: "7 String" },
         { id: 3, category: "8 String" },
-        { id: 4, category: "Single Cut" },
-        { id: 5, category: "Fanfret" },
-        { id: 6, category: "Headless" },
-        { id: 7, category: "Baritone" },
-        { id: 8, category: "Acoustic" },
+        { id: 4, category: "Acoustic" },
+        { id: 5, category: "Bass" },
     ];
 
     return (
